fae56263a2a736ba2e379d921364fa1637b4e46c
FierSet <66457209+FierSet@users.noreply.github.com>
2023-01-21 21:44:19 -0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,114 @@
+import React from 'react'
+import audio from './audio';
+import Colors from './Colors';
+
+
+function App()
+{
+    const [color, setcolor] = React.useState(0);
+    const [volumen, setvolumen] = React.useState(1);
+    const [label, setlabel] = React.useState("Sound.");
+    const [opened, setpoened] = React.useState(false);
+
+    const [power, setpower] = React.useState(true);
+      
+    React.useEffect(() => 
+    {
+        open();
+    }, []);
+
+    const havepower = () =>
+    {
+        if(document.getElementById('checked1').checked)
+            setpower(true);
+        else
+            setpower(false)
+    }
+
+    const open = () =>
+    {
+        if(!opened)
+            setcolor(Math.floor(Math.random() * Colors.length)); // sect position of array Colors
+        setpoened(true);
+    }
+    
+    return (
+        <div style = {{backgroundColor: Colors[color], minHeight: "100vh"}}>
+            <div className = 'text-center'>
+                <div className = 'container pt-5'>
+                    <div className = 'card'>
+
+                        <div className = 'card-header'><h2>Drum Machin.</h2></div>
+                        <div className = 'card-header'>
+                        <h1>Power: {power && 'On'} {!power && 'Off'}</h1>
+                        <label className = "switch">
+                            <input type = "checkbox" id = 'checked1' onClick = {havepower} defaultChecked></input>
+                            <span className ="slider"></span>
+                        </label>
+                        
+                        </div>
+                        <div className = 'card-body'>
+                            {audio.map((clip) => 
+                            (
+                                <Pad key = {clip.id} clip = {clip} volumen = {volumen} setlabel = {setlabel} />
+                            ))}
+                        </div>
+
+                        <div className = 'card-header'><h2>{label}</h2></div>
+                        <div className = 'card-body'>
+                            <input 
+                                type = 'range' 
+                                step = '0.01' 
+                                max = '1' 
+                                className='w-50' 
+                                value = {volumen} 
+                                onChange = 
+                                { (e) => 
+                                    {
+                                        setvolumen(e.target.value); 
+                                        setlabel("Volumen: " + parseInt(e.target.value * 100));
+                                    }
+                                } 
+                            />
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+function Pad ( {clip, volumen, setlabel} )
+{
+    const [active, setactive] = React.useState(false);
+
+    const havepower = () =>
+    {
+        overview();
+        if(document.getElementById('checked1').checked)
+            playsound();
+    }
+    const overview = () =>
+    {
+        setactive(true);
+        setTimeout(() => setactive(false), 200);
+    }
+    const playsound = () =>
+    {
+        const audiotag = document.getElementById(clip.keyTrigger);
+        audiotag.volume = volumen;
+        audiotag.currentTime = 0;
+        audiotag.play();
+        setlabel(clip.id);
+    }
+
+    return(
+        <div className = {`btn btn-secondary p-4 m-3 ${active && 'btn-warning'}`} onClick = {havepower}>
+            <audio className = 'clip' id = {clip.keyTrigger} src = {clip.url}/>
+            {clip.keyTrigger}
+        </div>
+    );
+}
+
+export default App;
